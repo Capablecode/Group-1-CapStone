e8e276be25a41fb003c37bf0ca3debac90eb57c2
Nuelle-tech <ogechinuelle5@gmail.com>
2024-12-31 19:01:48 +0100

    pushing codes to branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./Components/Header";
 import Highlight from "./Components/Highlight.jsx";
 import TabsComponent from "./Components/TabsComponent.jsx";
 import Footer from "./Components/Footer";
+import ProfilePage from "./Components/ProfileMobile.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProfilePage/>
       <Highlight />
       <TabsComponent />
       <Footer />
